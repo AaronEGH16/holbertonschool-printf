447de545d3bdce5d56d19c0f5146b493beb2def2
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-07-14 16:58:29 -0700

    The prototypes of all your functions should be included in your header

--- main.h
+++ main.h
@@ -1,4 +1,17 @@
 # ifndef MAIN_H
 # define MAIN_H
+# include <stdio.h>
+# include <stdarg.h>
+
+typedef struct prt
+{
+	char *flag;
+	int (*f)(va_list)
+} prt_t;
+
 int _printf(const char *format, ...);
+int get_flag(char *)(va_list);
+int prt_char(va_list pr);
+int prt_string(va_list pr);
+int prt_percent();
 # endif
