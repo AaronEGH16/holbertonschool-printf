ebc6b701ffe9eef2f27c23b28baa60095e8d7ac1
AaronEGH16 <aezegonz@gmail.com>
2023-07-14 18:29:27 -0700

    fixed name of variables and ;

--- main.h
+++ main.h
@@ -5,12 +5,12 @@
 
 typedef struct prt
 {
-	char *flag;
-	int (*f)(va_list)
+	char *fl;
+	int (*f)(va_list);
 } prt_t;
 
 int _printf(const char *format, ...);
-int get_flag(char *)(va_list);
+int (*get_flag(char *))(va_list);
 int prt_char(va_list pr);
 int prt_string(va_list pr);
 int prt_percent();
