915685422c2e7a5c6347f87d231f480cf6c12c84
AaronEGH16 <aezegonz@gmail.com>
2023-07-16 17:31:46 -0700

    added aux function and remove first version of %d

--- mandatory.c
+++ mandatory.c
@@ -50,19 +50,7 @@ int prt_string(va_list pr)
 
 int prt_decimal(va_list pr)
 {
-	int i = 0, num = va_arg(pr, int);
-	int len = get_lenght(num);
-	/*char str[len];
-
-	itoa(num, str, 10);
-
-	while (str[i] && str != NULL)
-	{
-		write(1, &str[i], 1);
-		i++;
-	}*/
-	printf("%d", len);
-	return (i);
+	return (0);
 }
 
 /**
@@ -75,20 +63,14 @@ int prt_decimal(va_list pr)
 
 int get_lenght(int num)
 {
-	int coc = 0, div = 1, con = 0;
+	int n = ((num < 0) ? (-(num)) : (num));
+	int r, con = 0;
 
-	while (coc != 1)
+	while (n > 0 && r != 0)
 	{
+		r = n % 10;
 		con++;
-		div = div * 10;
-		coc = num / div;
-
-		if (coc == 1)
-			return (con + 1);
-		if (coc < 1)
-			return (con);
-		if (num < 10)
-			return (1);
+		n = n / 10;
 	}
-	return (0);
+	return (con);
 }
