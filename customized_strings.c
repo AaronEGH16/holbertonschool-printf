d9cbc6e27802a16be8611982d2fa8317d8ebe15d
AaronEGH16 <aezegonz@gmail.com>
2023-07-22 16:01:50 -0700

    added S flag function

--- customized_strings.c
+++ customized_strings.c
@@ -46,3 +46,55 @@ int prt_addres(va_list pr)
 
 	return (i + 2);
 }
+
+/**
+ * prt_NPCascii - print the not printable characters
+ *
+ * @pr: addres of argument to print
+ *
+ * Return: count of chars printed
+ */
+
+int prt_NPCascii(va_list pr)
+{
+	char *str = va_arg(pr, char *);
+	int i = 0, j = 0, r = 0, h, temp;
+	unsigned long int aux;
+	char hexNum[100];
+
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] && str != NULL)
+	{
+		if ((str[i] > 0 && str[i] < 32) || (str[i] >= 127))
+		{
+			write(1, "\\", 1);
+			write(1, "x", 1);
+			aux = str[i];
+			h = 0;
+			while (aux > 0)
+			{
+				temp = aux % 16;
+				if (temp < 10)
+					temp = temp + 48;
+				else
+					temp = temp + 55;
+				hexNum[h++] = temp;
+				aux /= 16;
+			}
+			if (hexNum[h - 2] == '\0')
+			{
+				write(1, "0", 1);
+				r += 1;
+			}
+			for (j = (h - 1); j >= 0; j--)
+				write(1, &hexNum[j], 1);
+			i++;
+			r += h + 1;
+			continue;
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i + r);
+}
